d8ed7afc1372596ef4e738dfd72edecd6bfbb726
Vikram Singh <vikramuvs@gmail.com>
2021-12-03 16:41:32 +0530

    after 1st deploy with carousel nav buttons

--- components/Carousel.js
+++ components/Carousel.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
 
@@ -10,17 +10,18 @@ export const EmblaCarousel = () => {
   const [scrollSnaps, setScrollSnaps] = useState([]);
 
   const onSelect = useCallback(() => {
-    if (!embla) return;
-    setSelectedIndex(embla.selectedScrollSnap());
-    setPrevBtnEnabled(embla.canScrollPrev());
-    setNextBtnEnabled(embla.canScrollNext());
-  }, [embla, setSelectedIndex]);
+    if (!emblaApi) return;
+    setSelectedIndex(emblaApi.selectedScrollSnap());
+    setPrevBtnEnabled(emblaApi.canScrollPrev());
+    setNextBtnEnabled(emblaApi.canScrollNext());
+  }, [emblaApi, setSelectedIndex]);
 
   useEffect(() => {
-    if (emblaApi) {
-      // Embla API is ready
-    }
-  }, [emblaApi]);
+    if (!emblaApi) return;
+    onSelect();
+    setScrollSnaps(emblaApi.scrollSnapList());
+    emblaApi.on("select", onSelect);
+  }, [emblaApi, setScrollSnaps, onSelect]);
 
   const scrollPrev = useCallback(() => {
     if (emblaApi) emblaApi.scrollPrev();
@@ -30,8 +31,8 @@ export const EmblaCarousel = () => {
   }, [emblaApi]);
 
   return (
-    <div className="embla" ref={emblaRef}>
-      <div class="embla__viewport">
+    <div className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
         <div className="embla__container">
           <div className="embla__slide">
             <img
@@ -45,18 +46,25 @@ export const EmblaCarousel = () => {
         </div>
       </div>
       <button
-        class="embla__prev"
+        className="embla__button embla__button--prev"
         onClick={scrollPrev}
         disabled={!prevBtnEnabled}
       >
-        Prev
+        <svg
+          className="embla__button__svg"
+          viewBox="137.718 -1.001 366.563 644"
+        >
+          <path d="M428.36 12.5c16.67-16.67 43.76-16.67 60.42 0 16.67 16.67 16.67 43.76 0 60.42L241.7 320c148.25 148.24 230.61 230.6 247.08 247.08 16.67 16.66 16.67 43.75 0 60.42-16.67 16.66-43.76 16.67-60.42 0-27.72-27.71-249.45-249.37-277.16-277.08a42.308 42.308 0 0 1-12.48-30.34c0-11.1 4.1-22.05 12.48-30.42C206.63 234.23 400.64 40.21 428.36 12.5z" />
+        </svg>
       </button>{" "}
       <button
-        class="embla__next"
+        className="embla__button embla__button--next"
         onClick={scrollNext}
-        disabled={!prevBtnEnabled}
+        disabled={!nextBtnEnabled}
       >
-        Next
+        <svg className="embla__button__svg" viewBox="0 0 238.003 238.003">
+          <path d="M181.776 107.719L78.705 4.648c-6.198-6.198-16.273-6.198-22.47 0s-6.198 16.273 0 22.47l91.883 91.883-91.883 91.883c-6.198 6.198-6.198 16.273 0 22.47s16.273 6.198 22.47 0l103.071-103.039a15.741 15.741 0 0 0 4.64-11.283c0-4.13-1.526-8.199-4.64-11.313z" />
+        </svg>
       </button>
     </div>
   );
